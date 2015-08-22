37291736c35c903a89ee64a7f83943caafc731e7
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2015-08-23 01:13:52 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -12,8 +12,8 @@ module.exports = {
   },
   // Google OAuth Details
   google: {
-    clientID: "539462910404-foe1hp9cldrv8ts4209b37ean7jrcb1h.apps.googleusercontent.com",
-    clientSecret: "b1GcU6dt5V6UeIXWgqJ_YI2M",
+    clientID: "fillintheblanks",
+    clientSecret: "fillintheblanks",
     callbackURL: "/auth/google/callback"
   }
 }
